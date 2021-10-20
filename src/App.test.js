fd4e972282cca43777c8fcff5f90697331c5f004
Abhinav Batra <abhinavbatra2303@gmail.com>
2021-10-20 19:29:04 -0400

    Update Default Test
    Created a default test that can actually pass

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App.js', () => {
   render(<App />);
-  const linkElement = screen.getByText();
+  const linkElement = screen.getByText('Post a New Question!');
   expect(linkElement).toBeInTheDocument();
 });
