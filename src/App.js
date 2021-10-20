52648ce830c0907824da48c9fb63f743f8f1f577
Laila Alhalabi <70773705+lailaalhalabi@users.noreply.github.com>
2021-10-20 06:28:21 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import AskQuestionForm from './Components/AskQuestionForm';
 function App() {
   return (
     <div className="App">
@@ -18,6 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <AskQuestionForm></AskQuestionForm>
     </div>
   );
 }
