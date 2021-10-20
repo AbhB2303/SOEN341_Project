850406a42a12bf215db6e01ac7d2711905c610dc
Abhinav Batra <abhinavbatra2303@gmail.com>
2021-10-20 18:50:59 -0400

    Imported BootStrap and Created MenuBar
    - installed and imported bootstrap to index
    - imported Menu Bar and added to App.js
    -Designed Menu Bar with placeholder links for when each respective page is coded

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import AskQuestionForm from './components/AskQuestionForm'
+import MenuBar from './components/MenuBar'
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <MenuBar></MenuBar>
         <AskQuestionForm></AskQuestionForm>
       </div>
     )
---
+++ src/components/MenuBar.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class HeaderComponent extends Component {
+    render() {
+        return (
+            <header>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark text-light">
+                    <div className="navbar-brand">Ask Away</div>
+                        <ul className="navbar-nav">
+                            <li className = "nav-link">Home</li>
+                            <li className = "nav-link">Questions Forum</li>
+                            <li className = "nav-link">Profile Page</li>
+                            <li className = "nav-link">About</li>
+                            <li className = "nav-link">Content</li>
+                        </ul>
+                        <ul className="navbar-nav navbar-collapse justify-content-end">
+                            <li  className = "nav-link">Login</li>
+                            <li  className = "nav-link">Logout</li>
+                        </ul>
+                
+                </nav>
+            </header>
+        )
+    }
+}
+
+export default HeaderComponent;
