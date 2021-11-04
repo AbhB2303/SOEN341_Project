62ac6b67edcbc4d9dc8b0affdd4050347b66468d
Laila Alhalabi <70773705+lailaalhalabi@users.noreply.github.com>
2021-11-04 13:00:47 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Header from './components/Header'
 import AskQuestionForm from './components/AskQuestionForm'
+import ListQuestions from './components/ListQuestions'
+import Footer from './components/Footer';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AskQuestionForm></AskQuestionForm>
+      <div>
+        <Router>
+          <Header></Header>
+          <div className="container">
+            <AskQuestionForm></AskQuestionForm>
+            <Switch> http://localhost:3000/add-question
+              <Route path="/" component={ListQuestions}></Route>
+              <Route path="/questions" component={ListQuestions}></Route>
+              <Route path="/add-question" component={AskQuestionForm}></Route>
+              <ListQuestions></ListQuestions>
+            </Switch>
+          </div>
+          <Footer></Footer>
+        </Router>
       </div>
     )
   }
