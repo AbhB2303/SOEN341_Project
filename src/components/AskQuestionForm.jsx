bb568524afb475abbd44f77db9af6dad43dd0d8b
Laila Alhalabi <70773705+lailaalhalabi@users.noreply.github.com>
2021-11-04 12:58:22 -0400

    Add files via upload

---
+++ src/components/AskQuestionForm.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import QuestionsServices from '../Services/QuestionsServices';
+class AskQuestionForm extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            q_title: '',
+            q_text: ''
+        }
+        this.changeTitleHandler = this.changeTitleHandler.bind(this);
+        this.changeTextHandler = this.changeTextHandler.bind(this);
+        this.saveQuestion = this.saveQuestion.bind(this);
+    }
+    changeTitleHandler=(event) => {
+        this.setState({q_title: event.target.value});
+    }
+    changeTextHandler=(event) => {
+        this.setState({q_text: event.target.value});
+    }
+    saveQuestion = (q) => {
+        alert('Your question was posted successfully!');
+        q.preventDefault();
+        let question = {
+            q_text: this.state.q_text,
+            q_title: this.state.q_title
+        }
+        console.log('question => ' + JSON.stringify(question));
+        QuestionsServices.createQuestion(question);
+        window.location.reload();
+    }
+    render() {
+        return (
+            <div className="box">
+                <div className="h2">
+                    <h2>Post a New Question!</h2>
+                </div>
+                <form className="form" className="text-center">
+                    <input className="title" type="text" 
+                    placeholder="Enter the title of your question"
+                        name="q_title" id="q_title"
+                        value={this.state.q_title} 
+                        onChange={this.changeTitleHandler}
+                        required></input>
+                    <br /><br />
+                    <input className="question" type="text" 
+                    placeholder="Enter your question"
+                        name="q_text" id="q_text" 
+                        value={this.state.q_text}
+                        onChange={this.changeTextHandler} 
+                        required></input>
+                    <br /><br />
+                    <input className="submit" type="submit" value="Submit" 
+                    disabled={this.state.q_text.length<1 || this.state.q_title.length<1} onClick={this.saveQuestion}/>
+                    <button className="reset" type="reset">Reset</button>
+                    <br />
+                </form>
+            </div>
+        )
+    }
+}
+export default AskQuestionForm;
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <nav className="Header">
+            <div>
+              <a className="navbar-brand">Ask Away Website</a>
+            </div>
+          </nav>
+        </header>
+      </div>
+    )
+  }
+}
+export default Header;
