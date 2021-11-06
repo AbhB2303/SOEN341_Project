e6149af9e724b15de478620099e95202acdba64b
Laila Alhalabi <70773705+lailaalhalabi@users.noreply.github.com>
2021-11-06 16:59:07 -0400

    Update QuestionsServices.js

--- src/Services/QuestionsServices.js
+++ src/Services/QuestionsServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const QUESTIONS_API_BASE_URL = "http://localhost:8081/api/v1/question"
+const QUESTIONS_API_BASE_URL = "http://localhost:8080/api/v1/question"
 class QuestionsServices {
     getQuestions(){
         return axios.get(QUESTIONS_API_BASE_URL);
