8bacbdcb2f48c1351208784f1dc39767eaa6666c
Laila Alhalabi <70773705+lailaalhalabi@users.noreply.github.com>
2021-11-06 16:58:13 -0400

    Add files via upload

---
+++ springboot-backend/src/main/java/com/example/demo/SpringbootBackendApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+ * References: 
+ * (2021, May 08). Retrieved November 06, 2021, from https://www.youtube.com/watch?v=XkVpb_8IPUM&t=1825s
+ * */
+@SpringBootApplication
+public class SpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendApplication.class, args);
+	}
+}
