4aca2c32235044e0a750b7e326eaafa2b53950c5
Laila Alhalabi <70773705+lailaalhalabi@users.noreply.github.com>
2021-11-09 14:42:55 -0500

    Update UserRepository.java

--- springboot-backend/src/main/java/com/example/demo/repository/UserRepository.java
+++ springboot-backend/src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,9 +1,9 @@
-package soen341project.repository;
+package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import soen341project.model.User;
+import com.example.demo.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String>{
