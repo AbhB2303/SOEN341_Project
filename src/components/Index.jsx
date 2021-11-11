d17e94d3a241b4cfefcf8e2d1680da5c54adee24
Laila Alhalabi <70773705+lailaalhalabi@users.noreply.github.com>
2021-11-11 13:01:50 -0500

    Add files via upload

---
+++ src/components/Index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import AskQuestionForm from './AskQuestionForm'
+import ListQuestions from './ListQuestions'
+class Index extends Component {
+    render() {
+        return (
+            <div>
+                <AskQuestionForm></AskQuestionForm>
+                <ListQuestions></ListQuestions>
+            </div>
+        
+        );
+    }
+}
+
+export default Index;
---
+++ src/components/Register.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+class Register extends Component {
+    render() {
+        return (
+            <div className="Register">
+            <div className="h3">
+                <h3>Register to Continue!</h3>
+            </div>
+            <form className="RegisterForm">
+                <input className= "username" type="username" placeholder="Enter your username"
+                    name="username" id="username" required></input>
+                <br /><br />
+                <input className= "email" type="email" placeholder="Enter your email"
+                    name="email" id="email" required></input>
+                <br /><br />
+                <input className= "password" type="password" placeholder="Enter your password"
+                    name="password" id="password" required></input>
+                <br /><br />
+                <input className="submit" type="submit" value="Submit" onClick={this.submittionAlert} />
+                <br />
+            </form>
+        </div>
+        )
+    }
+}
+export default Register;
