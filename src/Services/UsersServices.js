2cfb17992791f6d8536bc48e0fdd0c31e2f1a11c
Laila Alhalabi <70773705+lailaalhalabi@users.noreply.github.com>
2021-11-18 12:13:33 -0500

    Add files via upload

---
+++ src/Services/UsersServices.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const USERS_API_BASE_URL = "http://localhost:8080/api/v1/users"
+class UsersServices {
+    createUser(u){
+        return axios.post(USERS_API_BASE_URL, u);
+    }
+}
+export default new UsersServices();
