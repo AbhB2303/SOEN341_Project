f3dcd3847ee2d882b2bcbdb1a66ebb149e4cb0c3
Stirl2608 <91553438+Stirl2608@users.noreply.github.com>
2021-11-26 10:55:38 -0500

    Update UsersServices.js

--- src/Services/UsersServices.js
+++ src/Services/UsersServices.js
@@ -1,8 +1,10 @@
 import axios from "axios";
-
 const USERS_API_BASE_URL = "http://localhost:8080/api/v1/users"
-class UsersServices {
-    createUser(u){
+
+class UsersServices
+{
+    createUser(u)
+    {
         return axios.post(USERS_API_BASE_URL, u);
     }
 }
