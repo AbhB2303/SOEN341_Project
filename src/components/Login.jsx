fcc661700366aa9f705553599da98b03ee3c87fe
Laila Alhalabi <70773705+lailaalhalabi@users.noreply.github.com>
2021-11-29 18:14:00 -0500

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
-class Login extends Component 
-{
-    render() 
-    {
+class Login extends Component {
+    render() {
         return (
             <div className="Login">
             <div className="h3">
                 <h3>Login to Continue!</h3>
             </div>
             <form className="LoginForm">
-                <input className ="email" type ="email" placeholder ="Enter your email"
-                    name ="email" id ="email" required></input>
+                <input className= "email" type="email" placeholder="Enter your email"
+                    name="email" id="email" required></input>
                 <br /><br />
-                <input className ="password" type ="password" placeholder ="Enter your password"
-                    name ="password" id ="password" required></input>
+                <input className= "password" type="password" placeholder="Enter your password"
+                    name="password" id="password" required></input>
                 <br /><br />
-                <input className ="submit" type ="submit" value ="Submit" />
+                <input className="submit" type="submit" value="Submit" onClick={this.submittionAlert} />
                 <br />
                 <p><strong>Forgot your password?</strong>  <a href="#"> click here!</a> </p>
             </form>
