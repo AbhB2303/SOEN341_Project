22e82897c52d297620455c45eb71659232232ebc
Laila Alhalabi <70773705+lailaalhalabi@users.noreply.github.com>
2021-11-29 18:06:19 -0500

    Update AnswerController.java

--- springboot-backend/src/main/java/com/example/demo/controller/AnswerController.java
+++ springboot-backend/src/main/java/com/example/demo/controller/AnswerController.java
@@ -3,14 +3,17 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Answer;
 import com.example.demo.repository.AnswerRepository;
 
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class AnswerController {
@@ -23,4 +26,11 @@ public class AnswerController {
 	public List<Answer> getAllAnswers(){
 		return answerRepository.findAll();
 	}
+	
+	//create new answer
+	@PostMapping("/answers")
+	public Answer createAnswer(@RequestBody Answer a) {
+		return answerRepository.save(a);
+	}
 }
+
