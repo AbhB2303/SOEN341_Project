8f0d3b719ba2869b4648e6d80dffefd85c7c4227
simrenmatharoo <35635891+simrenmatharoo@users.noreply.github.com>
2021-11-30 20:26:04 -0500

    Working version of current website

--- App.js
+++ App.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
-import React from 'react';
 import './App.css';
-import Footer from '.footer'
-import AskQuestionForm from './components/AskQuestionForm'
-
-class App extends Component 
-{
-  render() 
-  {
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Login from './components/Login'
+import Register from './components/Register'
+import ViewQuestion from './components/ViewQuestion'
+import Index from './components/Index'
+import Footer from './components/Footer'
+import NavBar from './components/Navbar'
+class App extends Component {
+  render() {
     return (
-      <div className="App">
-        <AskQuestionForm></AskQuestionForm>
+      <div>
+        <Router>
+         <NavBar></NavBar>
+          <div className="container">
+            <Switch> http://localhost:3000
+              <Route path="/" exact component={Index}></Route>
+              <Route path="/login" component={Login}></Route>
+              <Route path="/register" component={Register}></Route>
+              <Route path="/view-question/:id" component={ViewQuestion}></Route>
+            </Switch>
+          </div>
+          <Footer></Footer>
+        </Router>
       </div>
     )
   }
 }
 export default App;
-
-const App =()=>{
-  return (
-    <div className="App">
-      <Footer/>
-    </div>
-  )
-}
-export default App; 
