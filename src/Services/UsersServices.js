658b227b91769775d396d6a4b7a172fc617a800d
Laila Alhalabi <70773705+lailaalhalabi@users.noreply.github.com>
2021-12-01 14:42:56 -0500

    Update UsersServices.js

--- src/Services/UsersServices.js
+++ src/Services/UsersServices.js
@@ -1,11 +1,23 @@
 import axios from "axios";
 const USERS_API_BASE_URL = "http://localhost:8080/api/v1/users"
+const USERS_API_BASE_URL_LOGIN = "http://localhost:8080/api/v1/login"
+const USERS_API_BASE_URL_REGISTER = "http://localhost:8080/api/v1/register"
 
 class UsersServices
 {
     createUser(u)
     {
         return axios.post(USERS_API_BASE_URL, u);
+    }
+    loginUser(u) {
+        return axios.post(USERS_API_BASE_URL_LOGIN, u).then(response => {
+            return response
+          }).catch(error => console.log(error));
+    }
+    registerUser(u) {
+        return axios.post(USERS_API_BASE_URL_REGISTER, u).then(response => {
+            return response
+          }).catch(error => console.log(error));
     }
 }
 export default new UsersServices();
