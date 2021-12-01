5a799490ac28ed61ad610965166a3a4557b42d5f
simrenmatharoo <35635891+simrenmatharoo@users.noreply.github.com>
2021-11-30 23:02:51 -0500

    About Us and Contact Us pages done

---
+++ Navbar.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import * as ReactBootstrap from "react-bootstrap";
+
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ReactBootstrap.Navbar className="color-nav" variant="light" expand={false}>
+          <ReactBootstrap.Container fluid>
+            <ReactBootstrap.Navbar.Brand href="#">
+              Ask Away
+            </ReactBootstrap.Navbar.Brand>
+            <ReactBootstrap.Navbar.Toggle aria-controls="offcanvasNavbar" />
+            <ReactBootstrap.Navbar.Offcanvas
+              id="offcanvasNavbar"
+              aria-labelledby="offcanvasNavbarLabel"
+              placement="end"
+            >
+              <ReactBootstrap.Offcanvas.Header closeButton>
+                <ReactBootstrap.Offcanvas.Title id="offcanvasNavbarLabel">
+                  Ask Away
+                </ReactBootstrap.Offcanvas.Title>
+              </ReactBootstrap.Offcanvas.Header>
+              <ReactBootstrap.Offcanvas.Body>
+                <ReactBootstrap.Nav className="justify-content-end flex-grow-1 pe-3">
+                  <ReactBootstrap.Nav.Link href="/">
+                    Home
+                  </ReactBootstrap.Nav.Link>
+                  <ReactBootstrap.Nav.Link href="/login">
+                    Login
+                  </ReactBootstrap.Nav.Link>
+                  <ReactBootstrap.Nav.Link href="/register">
+                    Register
+                  </ReactBootstrap.Nav.Link>
+                  <ReactBootstrap.NavDropdown
+                    title="Dropdown"
+                    id="offcanvasNavbarDropdown"
+                  >
+                    <ReactBootstrap.NavDropdown.Item href="/aboutus">
+                      About Us
+                    </ReactBootstrap.NavDropdown.Item>
+                    <ReactBootstrap.NavDropdown.Item href="/contactus">
+                      Contact Us
+                    </ReactBootstrap.NavDropdown.Item>
+                    <ReactBootstrap.NavDropdown.Divider />
+                    <ReactBootstrap.NavDropdown.Item href="#action5">
+                      Privacy and Security
+                    </ReactBootstrap.NavDropdown.Item>
+                  </ReactBootstrap.NavDropdown>
+                </ReactBootstrap.Nav>
+                <ReactBootstrap.Form className="d-flex">
+                  <ReactBootstrap.FormControl
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <ReactBootstrap.Button variant="outline-success">
+                    Search
+                  </ReactBootstrap.Button>
+                </ReactBootstrap.Form>
+              </ReactBootstrap.Offcanvas.Body>
+            </ReactBootstrap.Navbar.Offcanvas>
+          </ReactBootstrap.Container>
+        </ReactBootstrap.Navbar>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
