e506bf58ab8362743409b0b850ade5400fc5bb8c
Stirl2608 <91553438+Stirl2608@users.noreply.github.com>
2021-12-03 10:57:04 -0500

    Update QuestionsServices.js

--- src/Services/QuestionsServices.js
+++ src/Services/QuestionsServices.js
@@ -1,14 +1,18 @@
 import axios from "axios";
 const QUESTIONS_API_BASE_URL = "http://localhost:8080/api/v1/question"
 
-class QuestionsServices {
-    getQuestions() {
+class QuestionsServices 
+{
+    getQuestions()
+    {
         return axios.get(QUESTIONS_API_BASE_URL);
     }
-    createQuestion(q) {
+    createQuestion(q)
+    {
         return axios.post(QUESTIONS_API_BASE_URL, q);
     }
-    getQuestionById(q_id) {
+    getQuestionById(q_id)
+    {
         return axios.get(QUESTIONS_API_BASE_URL + '/' + q_id);
     }
 }
