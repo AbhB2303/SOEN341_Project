c9db0bbc812faab5b1de58e0a106129b4b89ad92
Stirl2608 <91553438+Stirl2608@users.noreply.github.com>
2021-12-03 10:58:49 -0500

    Update reportWebVitals.js

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,5 +1,6 @@
 const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
+  if (onPerfEntry && onPerfEntry instanceof Function) 
+  {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
