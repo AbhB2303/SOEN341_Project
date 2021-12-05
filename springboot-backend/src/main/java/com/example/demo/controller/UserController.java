8c16a899598040405abb787a52166812a65a0416
Stirl2608 <91553438+Stirl2608@users.noreply.github.com>
2021-12-05 14:29:19 -0500

    Update UserController.java

--- springboot-backend/src/main/java/com/example/demo/controller/UserController.java
+++ springboot-backend/src/main/java/com/example/demo/controller/UserController.java
@@ -1,10 +1,8 @@
 package com.example.demo.controller;
-
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,49 +13,59 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
-public class UserController {
+public class UserController 
+{
 	@Autowired
 	private UserRepository userRepository;
 
 	// get all Users
 	@GetMapping("/users")
-	public List<User> getAllUsers() {
+	public List<User> getAllUsers() 
+	{
 		return userRepository.findAll();
 	}
 
 	// create new user
 	@PostMapping("/users")
-	public User createUser(@RequestBody User user) {
+	public User createUser(@RequestBody User user)
+	{
 		return userRepository.save(user);
 	}
 
 	@PostMapping("/login")
-    public String login(@RequestBody Map<String, String> json ) {
+  	public String login(@RequestBody Map<String, String> json )
+    	{
 		String username = json.get("username");
 		String password = json.get("password");
 		User u = userRepository.findByUsernameAndPassword(username, password);
-		if (u == null) {
+		if (u == null) 
+		{
 			return "fail";			
-		} else {
+		} 
+		else 
+		{
 			return username;
 		}
 	}
 	
 	@PostMapping("/register")
-        public String register(@RequestBody Map<String, String> json ) {
+        public String register(@RequestBody Map<String, String> json ) 
+	{
 		String username = json.get("username");
 		String password = json.get("password");
 		User u = userRepository.findByUsernameAndPassword(username, password);
-		if (u == null) {
+		if (u == null) 
+		{
 			return username;		
-		} else {
+		} 
+		else 
+		{
 			return "fail";
 		}
 	}
