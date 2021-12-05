9e7668f1e66500a4cf0c07cac965f29f04c39fde
Stirl2608 <91553438+Stirl2608@users.noreply.github.com>
2021-12-05 14:36:24 -0500

    Update AnswerRepository.java

--- springboot-backend/src/main/java/com/example/demo/repository/AnswerRepository.java
+++ springboot-backend/src/main/java/com/example/demo/repository/AnswerRepository.java
@@ -1,11 +1,10 @@
 package com.example.demo.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.example.demo.model.Answer;
 
 @Repository
-public interface AnswerRepository extends JpaRepository<Answer, Long>{
+public interface AnswerRepository extends JpaRepository<Answer, Long>
+{
 
 }
