af67e2d5433236e45e8c2599ceeedb150d279b5c
Stirl2608 <91553438+Stirl2608@users.noreply.github.com>
2021-12-05 14:34:24 -0500

    Update Question.java

--- springboot-backend/src/main/java/com/example/demo/model/Question.java
+++ springboot-backend/src/main/java/com/example/demo/model/Question.java
@@ -1,5 +1,4 @@
 package com.example.demo.model;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,19 +8,25 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "question")
-public class Question {
+public class Question 
+{
 	// variables
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long q_id;
+	
 	@Column(name = "q_title")
 	private String q_title;
+	
 	@Column(name = "q_text")
 	private String q_text;
+	
 	@Column(name = "username")
 	private String username;
+	
 	// constructors
-	public Question(String q_title, String q_text, String username) {
+	public Question(String q_title, String q_text, String username)
+	{
 		super();
 		this.q_title = q_title;
 		this.q_text = q_text;
@@ -30,31 +35,47 @@ public class Question {
 		else
 			this.username = username;
 	}
-	public Question () {
-	}
+	
+	public Question () {}
+	
 	// setters and getters
-	public long getId() {
+	public long getId() 
+	{
 		return q_id;
 	}
-	public void setId(long q_id) {
+	
+	public void setId(long q_id) 
+	{
 		this.q_id = q_id;
 	}
-	public String getQ_title() {
+	
+	public String getQ_title() 
+	{
 		return q_title;
 	}
-	public void setQ_title(String q_title) {
+	
+	public void setQ_title(String q_title) 
+	{
 		this.q_title = q_title;
 	}
-	public String getQ_text() {
+	
+	public String getQ_text() 
+	{
 		return q_text;
 	}
-	public void setQ_text(String q_text) {
+	
+	public void setQ_text(String q_text) 
+	{
 		this.q_text = q_text;
 	}
-	public String getUsername() {
+	
+	public String getUsername() 
+	{
 		return username;
 	}
-	public void setUsername(String username) {
+	
+	public void setUsername(String username)
+	{
 		this.username = username;
 	}
 }
