27d9c7f089feb1a4d53ed0c548ca75b5ea1d7b1e
Stirl2608 <91553438+Stirl2608@users.noreply.github.com>
2021-12-05 14:36:56 -0500

    Update UserRepository.java

--- springboot-backend/src/main/java/com/example/demo/repository/UserRepository.java
+++ springboot-backend/src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,11 +1,10 @@
 package com.example.demo.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.example.demo.model.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, Long>
+{
 	User findByUsernameAndPassword(String username, String passsword);
 }
