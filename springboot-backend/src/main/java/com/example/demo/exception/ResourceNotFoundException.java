0c3a13ca431089a7a034d12546dbd67b3ebe9164
Stirl2608 <91553438+Stirl2608@users.noreply.github.com>
2021-12-05 14:30:06 -0500

    Update ResourceNotFoundException.java

--- springboot-backend/src/main/java/com/example/demo/exception/ResourceNotFoundException.java
+++ springboot-backend/src/main/java/com/example/demo/exception/ResourceNotFoundException.java
@@ -1,16 +1,14 @@
 package com.example.demo.exception;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends RuntimeException{
-	
+public class ResourceNotFoundException extends RuntimeException
+{
 	private static final long sVUId = 1L;
 	
-	public ResourceNotFoundException (String msg) {
+	public ResourceNotFoundException (String msg)
+	{
 		super(msg);
-	}
-	
-	
+	}	
 }
